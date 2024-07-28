bdad2602d380bd2248919afeb3f209127f1f5e86
Dhanashri Hire <dhanashri.hire23@outlook.com>
2024-07-28 16:01:13 +0530

    first commit

---
+++ Functions.js
@@ -0,0 +1,13 @@
+//Write a function that takes two numbers as arguments and returns their sum.
+//Write both a regular function and an arrow function.
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const addArrow = (a, b) => a + b;
+  
+  console.log(add(5, 10)); // 15
+  console.log(addArrow(5, 10)); // 15
+ 
+  
---
+++ OperatorCondationalstmt.js
@@ -0,0 +1,38 @@
+// Arithmetic operations
+let a = 10, b = 20;
+console.log(a + b, a - b, a * b, a / b, a % b);
+
+// If-Else statement
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is not greater than b");
+}
+
+// Switch statement
+switch (a) {
+  case 10:
+    console.log("a is 10");
+    break;
+  default:
+    console.log("a is not 10");
+}
+
+// For loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// While loop
+let j = 0;
+while (j < 5) {
+  console.log(j);
+  j++;
+}
+
+// Do-While loop
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k < 5);
